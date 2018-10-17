8e169fe4e9645db4a9bf08320a07f5365036b4ca
dunia98 <44101849+dunia98@users.noreply.github.com>
2018-10-17 22:37:31 +0300

    Add files via upload

---
+++ _Practise1-A.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+
+name=input("Enter your name :")
+num1=input("Enter first number :")
+num2=input("Enter second number :")
+print( "Hi Mr/Mrs ",name)
+print("The sum of ",num1,"and ",num2,"is:",int(num1)+int(num2))
+print("The sub of",num1,"and",num2,"is:",int(num1)-int(num2))
+print("The mul of",num1,"and",num2,"is:",int(num1)*int(num2))
+print("The div of",num1,"over",num2,"is:",int(num1)/int(num2))
+print("The Reminder of",num1,"over",num2,"is:",int(num1)%int(num2))
+print("The Exponent of",num1,"to the power of",num2,"is:",int(num1)**int(num2))
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
